29b01742cad8d2df54f616b749cb016bbde9057c
Midori Fukami <midori.fukami@hotmail.com>
2022-02-08 00:34:04 -0300

    add starting project

---
+++ 01-starting-project/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1> My Todos </h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Learn Flutter" />
+    </div>
+  );
+}
+
+export default App;
