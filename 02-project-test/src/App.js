70234baea0f561013adc449824e8eacee28681b3
Midori Fukami <midori.fukami@hotmail.com>
2022-04-08 00:49:32 -0300

    add config prettier and esl

--- 02-project-test/src/App.js
+++ 02-project-test/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <div className="App">
       Start here.
+      <h3>teste</h3>
+      <GlobalStyle />
     </div>
   );
 }
